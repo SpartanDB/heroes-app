16b8d70ad9634425f96b75e00c5ff72d925d374b
Daniel Borrome <dborrome@gmail.com>
2024-02-16 10:20:03 -0400

    Agrega estilos y campos de formulario en las páginas de inicio de sesión y registro

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
+
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { MaterialModule } from '../material/material.module';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
   declarations: [
     LayoutPageComponent,
     LoginPageComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
   ],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [AuthRoutingModule, CommonModule, MaterialModule],
 })
 export class AuthModule {}
