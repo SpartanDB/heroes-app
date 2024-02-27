c52a8da10ad77ebdb465c9799f74576509ff04db
Daniel Borrome <dborrome@gmail.com>
2024-02-27 10:44:07 -0400

    Añadida funcionalidad de inicio de sesión y cierre de sesión

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+import { environment } from '../../../environments/environments';
+import { User } from '../interfaces/user.interface';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  private baseUrl = environment.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) {}
+
+  get currentUser(): User | undefined {
+    if (!this.user) return undefined;
+
+    return structuredClone(this.user);
+  }
+
+  login(email: string, password: string): Observable<User> {
+    //http.post('login', {email, password})
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`).pipe(
+      tap((user) => (this.user = user)),
+      tap((user) =>
+        localStorage.setItem('token', '3f5a7c9b1d2e4a6b8f0e1d3c5b6a7f8e')
+      )
+    );
+  }
+
+  logout(): void {
+    this.user = undefined;
+    localStorage.clear();
+  }
+}
